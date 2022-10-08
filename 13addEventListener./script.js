1604433f93538a5df10cea8d569e9696b4c757bf
Daniel Romero <daanmaro99june@gmail.com>
2022-10-08 00:43:38 -0500

    Usando preventDefault() en lso forms de JS

--- 13addEventListener./script.js
+++ 13addEventListener./script.js
@@ -1,7 +1,7 @@
 const h3 = document.querySelector('h3');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
-
+const form = document.querySelector('#form')
 const btn = document.querySelector('#btnCalcular');
 const btnClean = document.querySelector('#btnClean');
 const pResult = document.querySelector('#result');
@@ -9,7 +9,7 @@ const pResult = document.querySelector('#result');
 //*Use 'addEventListener'
 // element.addEventListner('evento', code JS)
 
-btn.addEventListener('click', sumar);
+form.addEventListener('click', sumar);
 btnClean.addEventListener('click', clean);
 //                            ⬆️ Solo se pone la función sin parentésis 
 /* A diferencia de cuando lo llamamos con HTML, ya que, onclick es específico lo que hay que llamar
@@ -17,7 +17,15 @@ btnClean.addEventListener('click', clean);
     va a escuchar, y posterior que función va a llamar cuando este evento ocurra, así le va agregar
     unos parentéisis para ejecutarla. */
 
-function sumar() {
+function sumar(event) {
+    // Método que llamamos para no ejectuar los métodos por defecto, como submit con los forms,
+    // usando el evento de form
+
+    /* Otra forma es indicar al útlimo botón del form  que se ade typeof button y no submit
+        ya que, es el método que usualmente esta por defecto, por lo tanto envía el form. */
+
+    console.log({event});
+    event.preventDefault();
     const suma = parseInt(input1.value) + (+input2.value);
     pResult.innerText = `Resultado: ${suma}`;
     console.log(suma);
